e20c770957e4f25fde41abd83088c5e5fa211a95
mrGreatProgrammer <fhgf9048@gmail.com>
2023-03-26 19:32:37 +0500

    adaptive and burger menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,7 @@
-const currentYearEl = document.querySelector('time');
+const currentYearEl = document.querySelector("time");
 const currentDate = new Date().getFullYear();
 
-currentYearEl.textContent= currentDate;
-
+currentYearEl.textContent = currentDate;
 
 // SLIDER
 $(".center").slick({
@@ -13,8 +12,25 @@ $(".center").slick({
   slidesToScroll: 3,
   autoplay: true,
   autoplaySpeed: 2500,
-  variableWidth: true
-  
+  variableWidth: true,
 });
 
+const burgerBtn = document.querySelector(".burger__btn");
+const burgerMenu = document.querySelector(".burger__menu");
 
+const isBurgerOpened = false;
+
+function toggleVisibleBurger() {
+  if (burgerBtn.classList.contains("burger__btn--opened")) {
+    burgerBtn.classList.remove("burger__btn--opened");
+    burgerMenu.classList.remove("burger__menu--opened");
+    burgerBtn.classList.add("burger__btn--closed");
+    burgerMenu.classList.add("burger__menu--closed");
+  } else {
+    burgerBtn.classList.add("burger__btn--opened");
+    burgerMenu.classList.add("burger__menu--opened");
+    burgerBtn.classList.remove("burger__btn--closed");
+    burgerMenu.classList.remove("burger__menu--closed");
+  }
+  console.log("clicked");
+}
